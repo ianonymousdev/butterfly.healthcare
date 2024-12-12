add86833e5a9b9ff0840cfe79744424761cae8a7
Swapwise Admin <mohd.irfan@gmail.com>
2024-12-13 01:47:13 +1100

    Initial commit: Project setup with ASP.NET Core, Serilog logging, and XUnit for testing. Includes basic API structure for mathematical operations.

--- Butterfly.Calculator.Services/obj/Debug/netcoreapp3.1/Butterfly.Calculator.Services.AssemblyInfo.cs
+++ Butterfly.Calculator.Services/obj/Debug/netcoreapp3.1/Butterfly.Calculator.Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Butterfly.Calculator.Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+291589b4c8becde56a30114af9d9d6fc57a56ee0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Butterfly.Calculator.Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Butterfly.Calculator.Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Butterfly.Calculator.Tests/obj/Debug/netcoreapp3.1/Butterfly.Calculator.Tests.AssemblyInfo.cs
+++ Butterfly.Calculator.Tests/obj/Debug/netcoreapp3.1/Butterfly.Calculator.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Butterfly.Calculator.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+291589b4c8becde56a30114af9d9d6fc57a56ee0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Butterfly.Calculator.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Butterfly.Calculator.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
